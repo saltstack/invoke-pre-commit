a6e665840dde135490b81c0a37fd52ae0dce8c8b
Pedro Algarvio <pedro@algarvio.me>
2021-06-28 09:53:46 +0100

    Mirror: 1.5.0

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
     scripts=["invoke-wrapper"],
-    install_requires=['invoke==1.4.1'],
+    install_requires=['invoke==1.5.0'],
 )
